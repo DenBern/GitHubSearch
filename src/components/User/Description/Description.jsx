764fadf7f8d1b2eeedd7071385d39da6ab2c3a6b
Bernovich Denis <denisbernovich@gmail.com>
2023-04-06 23:02:57 +0300

    fix: error 404

--- src/components/User/Description/Description.jsx
+++ src/components/User/Description/Description.jsx
@@ -3,35 +3,39 @@ import { Spinner } from "../../Spinner/Spinner.jsx";
 
 import './Description.scss';
 
-export const Description = (props) => {
-  const {avatar, name, login, url, followers, following, loading } = props;
+import userphoto from "../../../assets/png/not_found_user_icon.png"
+
+export const Description = ({avatar, name, login, url, followers, following, loading }) => {
   
   return (
     <div className="description">
-      {loading && <Spinner/>}
-      <div
-        className="photo"
-        style={{backgroundImage: `url(${avatar})`}}>
-      </div>
-      <div className="name-link">
-        <p className="name">{!name ? 'Name is empty' : name}</p>
-        <a 
-          href={url}
-          target="_blank"
-          className="link">
-          {login}
-        </a>
-      </div>
-      <div className="followers-following">
-        <div className="followers">
-          <div className="followers-icon"/>
-          <span>{`${followers > 1000 ? (followers / 1000).toFixed(1) + 'k' : followers} followers`}</span>
-        </div>
-        <div className="following">
-          <div className="following-icon"/>
-          <span>{`${following > 1000 ? (following / 1000).toFixed(1) + 'k' : following}`} following</span>
-        </div>
-      </div>
+      {loading ? <Spinner/> : (
+        <>
+          <div
+            className="photo"
+            style={{backgroundImage: `url(${!avatar ? userphoto : avatar})`}}>
+          </div>
+          <div className="name-link">
+            <p className="name">{!name ? 'Name is empty' : name}</p>
+            <a 
+              href={url}
+              target="_blank"
+              className="link">
+              {login}
+            </a>
+          </div>
+          <div className="followers-following">
+            <div className="followers">
+              <div className="followers-icon"/>
+              <span>{`${followers > 1000 ? (followers / 1000).toFixed(1) + 'k' : followers} followers`}</span>
+            </div>
+            <div className="following">
+              <div className="following-icon"/>
+              <span>{`${following > 1000 ? (following / 1000).toFixed(1) + 'k' : following}`} following</span>
+            </div>
+          </div>
+          </>
+          )}
     </div>
   )
 }
